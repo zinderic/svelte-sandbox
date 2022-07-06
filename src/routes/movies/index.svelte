ee4e122ec618682f13746bae5ecd9e262065c428
Kalin Staykov <kstaykov@vmware.com>
2022-07-06 14:07:00 +0300

    more movies and styling

--- src/routes/movies/index.svelte
+++ src/routes/movies/index.svelte
@@ -5,6 +5,22 @@
 			"Name": "Morbius",
 			"Poster": "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/6JjfSchsU6daXk2AKX8EEBjO3Fm.jpg"
 		},
+		{
+			"Name": "Top Gun Maverick",
+			"Poster": "https://www.themoviedb.org/t/p/w1280/kqVncEM5t00mbaNR0UB1iAz7nOi.jpg"
+		},
+		{
+			"Name": "The Ledge",
+			"Poster": "https://www.themoviedb.org/t/p/w1280/dHKfsdNcEPw7YIWFPIhqiuWrSAb.jpg"
+		},
+		{
+			"Name": "Dark Cloud",
+			"Poster": "https://www.themoviedb.org/t/p/w1280/q1ngkvPCnOYKeyNZjg8QqvaSnuZ.jpg"
+		},
+		{
+			"Name": "Heatwave",
+			"Poster": "https://www.themoviedb.org/t/p/w1280/AeeXeyiiPOyybnjSnJoSTixOZQq.jpg"
+		}
 	]
 	
 </script>
@@ -18,23 +34,25 @@
 <article class="movies">
 	<div class="movie">
 		{#each movies as movie }
+		<div>
 			<p>{movie.Name}</p>
 			<img src={movie.Poster} alt=""/>
+		</div>
 		{/each}
 	</div>
 </article>
 
 <style>
-	/* TODO fix the poster size by wrapping it in something */
 	.movies {
 		display: flex;
-		margin: auto;
+		flex-direction: row;
+		flex-flow: column wrap;
 	}
 	.movie {
 		display: grid;
-		grid-template-columns: 2rem 1fr 2rem;
+		grid-template-columns: 0.5fr 0.5fr 0.5fr;
 		grid-gap: 0.5rem;
-		align-items: center;
+		align-items: baseline;
 		margin: 0 0 0.5rem 0;
 		padding: 0.5rem;
 		background-color: white;
@@ -42,5 +60,8 @@
 		filter: drop-shadow(2px 4px 6px rgba(0, 0, 0, 0.1));
 		transform: translate(-1px, -1px);
 		transition: filter 0.2s, transform 0.2s;
+	}
+	.movie img {
+		height: 15rem;
 	}
 </style>
